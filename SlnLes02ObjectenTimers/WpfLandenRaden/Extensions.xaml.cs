4083988c50a116dd8eda956a0a3c93b7515f2780
Mohamed-Bn <114338202+Mohamed-Bn@users.noreply.github.com>
2024-03-05 20:00:32 +0100

    les2

---
+++ SlnLes02ObjectenTimers/WpfLandenRaden/Extensions.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Media;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Threading;
+
+namespace WpfLandenRaden
+{
+
+    public static class Extensions
+    {
+        private static Random rng = new Random();
+
+        public static void Shuffle<T>(this IList<T> list)
+        {
+            int n = list.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = rng.Next(n + 1);
+                T value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
+        }
+    }
+}
