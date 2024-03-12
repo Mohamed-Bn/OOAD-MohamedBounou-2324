c212b1f342b46f02ab0c236a6c93d554e6af0fce
Mohamed-Bn <114338202+Mohamed-Bn@users.noreply.github.com>
2024-03-12 15:58:50 +0100

    WpfJordy + WpfVcardEditor

---
+++ SlnLes04WpfLayout/WpfVcardEditor/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfVcardEditor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void About_Click(object sender, RoutedEventArgs e)
+        {
+            new AboutWindow().Show();
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            new ExitWindow().Show();
+        }
+    }
+}
