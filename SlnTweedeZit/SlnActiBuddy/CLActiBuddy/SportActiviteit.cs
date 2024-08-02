299a6365ab01073eec58abcff95d9f26cbe524eb
Mohamed-Bn <mohamed.bounou@student.odisee.be>
2024-08-03 00:16:55 +0200

    Create SportActiviteit.cs

---
+++ SlnTweedeZit/SlnActiBuddy/CLActiBuddy/SportActiviteit.cs
@@ -0,0 +1,15 @@
+﻿namespace CLActiBuddy
+{
+    public class SportActiviteit : Activiteit
+    {
+        public ActiviteitMoeilijkheid? Moeilijkheid { get; set; }
+    }
+
+    public enum ActiviteitMoeilijkheid
+    {
+        Nvt,
+        Makkelijk,
+        Gemiddeld,
+        Zwaar
+    }
+}
