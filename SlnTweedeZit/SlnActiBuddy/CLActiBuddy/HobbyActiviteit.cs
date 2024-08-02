a66a6c737ebfd8ad480ad38fb873fe7e1f092f69
Mohamed-Bn <mohamed.bounou@student.odisee.be>
2024-08-02 22:10:32 +0200

    Create HobbyActiviteit.cs

---
+++ SlnTweedeZit/SlnActiBuddy/CLActiBuddy/HobbyActiviteit.cs
@@ -0,0 +1,15 @@
+﻿namespace CLActiBuddy
+{
+    public class HobbyActiviteit : Activiteit
+    {
+        public ActiviteitNiveau? Niveau { get; set; }
+    }
+
+    public enum ActiviteitNiveau
+    {
+        Nvt,
+        Beginner,
+        Halfgevorderd,
+        Gevorderd
+    }
+}
