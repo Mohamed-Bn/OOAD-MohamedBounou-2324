259c191f4e3c9655d892e8b20cde664f168eef53
Mohamed-Bn <mohamed.bounou@student.odisee.be>
2024-08-03 18:54:42 +0200

    Update PersonenOverzichtPage.xaml.cs

--- SlnTweedeZit/SlnActiBuddy/WpfAdmin/PersoonPage/PersonenOverzichtPage.xaml.cs
+++ SlnTweedeZit/SlnActiBuddy/WpfAdmin/PersoonPage/PersonenOverzichtPage.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows.Controls;
+using CLActiBuddy;
 
 namespace WpfAdmin.PersoonPage
 {
@@ -20,9 +8,67 @@ namespace WpfAdmin.PersoonPage
     /// </summary>
     public partial class PersonenOverzichtPage : Page
     {
+        private List<Persoon> personen = new List<Persoon>();
         public PersonenOverzichtPage()
         {
             InitializeComponent();
+            personen = Persoon.GetAllPersonen();
+
+            LstPersonen.Items.Clear();
+            foreach (Persoon persoon in personen)
+            {
+                LstPersonen.Items.Add(persoon);
+            }
+        }
+
+        private void LstPersonen_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (LstPersonen.SelectedItem == null)
+            {
+                LblLogin.Text = "";
+                LblRegDatum.Text = "";
+                LblIsAdmin.Text = "";
+                ImgPersoonFoto.Source = null;
+            }
+            else
+            {
+                Persoon person = (Persoon)LstPersonen.SelectedItem;
+                LblLogin.Text = person.Login;
+                LblRegDatum.Text = person.RegDatum.ToLongDateString();
+                LblIsAdmin.Text = person.IsAdmin ? "Ja" : "Nee";
+                ImgPersoonFoto.Source = MainWindow.ByteToImage(person.Profielfoto);
+            }
+        }
+
+        private void NieuwButton_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new PersoonAanmakenPage());
+        }
+
+        private void VerwijderenButton_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            if (LstPersonen.SelectedItem == null)
+            {
+                LblError.Content = "Selecteer een persoon om te verwijderen.";
+            }
+            else
+            {
+                Persoon persoon = (Persoon)LstPersonen.SelectedItem;
+                NavigationService.Navigate(new PersoonVerwijderenPage(persoon));
+            }
+        }
+
+        private void BewerkenButton_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            if (LstPersonen.SelectedItem == null)
+            {
+                LblError.Content = "Selecteer een persoon om te bewerken.";
+            }
+            else
+            {
+                Persoon persoon = (Persoon)LstPersonen.SelectedItem;
+                NavigationService.Navigate(new PersoonBewerkenPage(persoon));
+            }
         }
     }
 }
