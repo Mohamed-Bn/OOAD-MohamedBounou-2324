1a7ec01bd64f11474d0b690fefd730e6d486d1b3
Mohamed-Bn <mohamed.bounou@student.odisee.be>
2024-08-03 18:33:47 +0200

    Update LoginWindow.xaml.cs

--- SlnTweedeZit/SlnActiBuddy/WpfAdmin/LoginWindow.xaml.cs
+++ SlnTweedeZit/SlnActiBuddy/WpfAdmin/LoginWindow.xaml.cs
@@ -1,16 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using CLActiBuddy;
 
 namespace WpfAdmin
 {
@@ -22,6 +11,64 @@ namespace WpfAdmin
         public LoginWindow()
         {
             InitializeComponent();
+            try
+            {
+                DataInitializerService.InitializeData();
+            }
+            catch (Exception ex)
+            {
+                LblErrorMessage.Content = $"Er ging iets mis bij het initialiseren van de data. Error: {ex.Message}";
+                throw;
+            }
+        }
+
+        private void BtnInloggen_Click(object sender, RoutedEventArgs e)
+        {
+            string gebruikersnaam = TxtGebruikersnaam.Text;
+            string paswoord = TxtWachtwoord.Password;
+
+            if (string.IsNullOrEmpty(gebruikersnaam))
+            {
+                LblErrorMessage.Content = "Vul gebruikersnaam in!";
+                return;
+            }
+            if (string.IsNullOrEmpty(paswoord))
+            {
+                LblErrorMessage.Content = "Vul paswoord in!";
+                return;
+            }
+
+            paswoord = PasswordHashService.QuickHash(paswoord);
+
+            try
+            {
+                Persoon persoon = Persoon.GetByGebruikersnaamEnPaswoord(gebruikersnaam, paswoord);
+                if (persoon == null || !persoon.IsAdmin)
+                {
+                    LblErrorMessage.Content = "Incorrecte login.";
+                }
+                else
+                {
+                    Application.Current.Properties.Add("persoon", persoon);
+
+                    if (Application.Current.Properties.Contains("loginPagina"))
+                    {
+                        Application.Current.Properties.Remove("loginPagina");
+                    }
+
+                    MainWindow mainWindow = new MainWindow()
+                    {
+                        Title = $"WPF Admin - welkom {persoon.Voornaam} {persoon.Achternaam}"
+                    };
+                    mainWindow.Show();
+
+                    Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                LblErrorMessage.Content = $"Error: {ex.Message}";
+            }
         }
     }
 }
