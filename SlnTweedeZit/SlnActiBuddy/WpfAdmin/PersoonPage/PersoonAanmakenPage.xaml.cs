06ee1ccfa4a59aee5450e02b0495e67381b3a0f1
Mohamed-Bn <mohamed.bounou@student.odisee.be>
2024-08-03 18:54:46 +0200

    Update PersoonAanmakenPage.xaml.cs

--- SlnTweedeZit/SlnActiBuddy/WpfAdmin/PersoonPage/PersoonAanmakenPage.xaml.cs
+++ SlnTweedeZit/SlnActiBuddy/WpfAdmin/PersoonPage/PersoonAanmakenPage.xaml.cs
@@ -1,17 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.IO;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
+using CLActiBuddy;
+using Microsoft.Win32;
 
 namespace WpfAdmin.PersoonPage
 {
@@ -24,5 +17,85 @@ namespace WpfAdmin.PersoonPage
         {
             InitializeComponent();
         }
+
+        Persoon nieuwePersoon = new Persoon();
+
+        private void BtnKiesFoto_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Multiselect = false,
+                Filter = "Image files (*.png;*.jpeg;*.jpg)|*.png;*.jpeg;*.jpg|All files (*.*)|*.*"
+            };
+
+            bool? result = openFileDialog.ShowDialog();
+
+            if (result == true)
+            {
+                try
+                {
+                    Uri fileUri = new Uri(openFileDialog.FileName);
+                    BitmapImage bitmapImage = new BitmapImage();
+
+                    bitmapImage.BeginInit();
+                    bitmapImage.UriSource = fileUri;
+                    bitmapImage.DecodePixelHeight = 200;
+                    bitmapImage.EndInit();
+
+                    ImgPersoonFoto.Source = bitmapImage;
+                    nieuwePersoon.Profielfoto = File.ReadAllBytes(openFileDialog.FileName);
+                }
+                catch (Exception ex)
+                {
+                    LblError.Content = $"Error: {ex.Message}";
+                }
+            }
+        }
+
+        private void BtnOpslaan_Click(object sender, RoutedEventArgs e)
+        {
+            string voornaam = TxtVoornaam.Text;
+            string achternaam = TxtAchternaam.Text;
+            string login = TxtLogin.Text;
+            string paswoord = TxtPasswordBox.Password;
+            bool isAdmin = ChkIsAdmin.IsChecked == true;
+
+            if (string.IsNullOrEmpty(voornaam) ||
+                string.IsNullOrEmpty(achternaam) ||
+                string.IsNullOrEmpty(login) ||
+                string.IsNullOrEmpty(paswoord))
+            {
+                LblError.Content = "Alle velden zijn verplicht!";
+                return;
+            }
+            if (nieuwePersoon.Profielfoto == null)
+            {
+                LblError.Content = "Selecteer een profielfoto!";
+                return;
+            }
+
+            nieuwePersoon.Voornaam = voornaam;
+            nieuwePersoon.Achternaam = achternaam;
+            nieuwePersoon.Login = login;
+            nieuwePersoon.Paswoord = PasswordHashService.QuickHash(paswoord);
+            nieuwePersoon.IsAdmin = isAdmin;
+            nieuwePersoon.RegDatum = DateTime.Now;
+
+            try
+            {
+                nieuwePersoon.InsertInDb();
+                NavigationService.Navigate(new PersonenOverzichtPage());
+            }
+            catch (Exception ex)
+            {
+                LblError.Content = $"Error: {ex.Message}";
+            }
+        }
+
+        private void BtnAnnuleren_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new PersonenOverzichtPage());
+        }
     }
 }
+
