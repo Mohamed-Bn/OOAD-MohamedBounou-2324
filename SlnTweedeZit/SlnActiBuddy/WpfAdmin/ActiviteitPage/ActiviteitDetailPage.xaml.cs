7c9d91837e088cae85e1b8ab7087d5765b97adef
Mohamed-Bn <mohamed.bounou@student.odisee.be>
2024-08-03 17:47:24 +0200

    ActiviteitDetailPage.xaml.cs

--- SlnTweedeZit/SlnActiBuddy/WpfAdmin/ActiviteitPage/ActiviteitDetailPage.xaml.cs
+++ SlnTweedeZit/SlnActiBuddy/WpfAdmin/ActiviteitPage/ActiviteitDetailPage.xaml.cs
@@ -1,17 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
+using CLActiBuddy;
 
 namespace WpfAdmin.ActiviteitPage
 {
@@ -20,9 +10,43 @@ namespace WpfAdmin.ActiviteitPage
     /// </summary>
     public partial class ActiviteitDetailPage : Page
     {
-        public ActiviteitDetailPage()
+        public ActiviteitDetailPage(Activiteit activiteit)
         {
             InitializeComponent();
+
+            // alle info vullen en tonen
+            TxtTitel.Text = activiteit.Titel;
+            TxtBeschrijving.Text = activiteit.Beschrijving;
+            TxtDatum.Text = activiteit.DatumTijd.ToLongDateString() + " " + activiteit.DatumTijd.ToShortTimeString();
+            if (activiteit.Icoon != null)
+            {
+                ImgIcoon.Source = MainWindow.ByteToImage(activiteit.Icoon);
+            }
+            TxtLongitude.Text = activiteit.Longitude.ToString();
+            TxtLaltitude.Text = activiteit.Latitude.ToString();
+            TxtMaxPersonen.Text = activiteit.MaxPersonen.ToString();
+            TxtSoort.Text = activiteit.Soort.ToString();
+            TxtLeeftijdsgroep.Text = activiteit.Leeftijdsgroep.ToString();
+            if (activiteit.Soort == ActiviteitSoort.Hobby)
+            {
+                LblSpecifiek.Content = "Niveau";
+                TxtSpecifiek.Text = ((HobbyActiviteit)activiteit).Niveau?.ToString() ?? "";
+            }
+            else if (activiteit.Soort == ActiviteitSoort.Cultuur)
+            {
+                LblSpecifiek.Content = "Sector";
+                TxtSpecifiek.Text = ((CultuurActiviteit)activiteit).Sector?.ToString() ?? "";
+            }
+            else if (activiteit.Soort == ActiviteitSoort.Sport)
+            {
+                LblSpecifiek.Content = "Moeilijkheid";
+                TxtSpecifiek.Text = ((SportActiviteit)activiteit).Moeilijkheid?.ToString() ?? "";
+            }
+        }
+
+        private void BtnTerug_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new ActiviteitenOverzichtPage());
         }
     }
 }
