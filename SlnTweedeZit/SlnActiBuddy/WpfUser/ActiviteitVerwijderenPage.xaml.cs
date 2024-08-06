9f2b8018485c48e9aac002cf16ce8126f2e8dc28
Mohamed-Bn <mohamed.bounou@student.odisee.be>
2024-08-06 21:41:37 +0200

    Update ActiviteitVerwijderenPage.xaml.cs

--- SlnTweedeZit/SlnActiBuddy/WpfUser/ActiviteitVerwijderenPage.xaml.cs
+++ SlnTweedeZit/SlnActiBuddy/WpfUser/ActiviteitVerwijderenPage.xaml.cs
@@ -1,17 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
+using CLActiBuddy;
 
 namespace WpfUser
 {
@@ -20,9 +10,57 @@ namespace WpfUser
     /// </summary>
     public partial class ActiviteitVerwijderenPage : Page
     {
-        public ActiviteitVerwijderenPage()
+        Activiteit activiteit = null;
+        public ActiviteitVerwijderenPage(Activiteit activiteit)
         {
             InitializeComponent();
+            this.activiteit = activiteit;
+
+            TxtTitel.Text = activiteit.Titel;
+            TxtBeschrijving.Text = activiteit.Beschrijving;
+            TxtDatum.Text = activiteit.DatumTijd.ToLongDateString() + " " + activiteit.DatumTijd.ToShortTimeString();
+            if (activiteit.Icoon != null)
+            {
+                ImgIcoon.Source = MainWindow.ByteToImage(activiteit.Icoon);
+            }
+            TxtLongitude.Text = activiteit.Longitude.ToString();
+            TxtLaltitude.Text = activiteit.Latitude.ToString();
+            TxtMaxPersonen.Text = activiteit.MaxPersonen.ToString();
+            TxtSoort.Text = activiteit.Soort.ToString();
+            TxtLeeftijdsgroep.Text = activiteit.Leeftijdsgroep.ToString();
+            if (activiteit.Soort == ActiviteitSoort.Hobby)
+            {
+                LblSpecifiek.Content = "Niveau";
+                TxtSpecifiek.Text = ((HobbyActiviteit)activiteit).Niveau?.ToString() ?? "";
+            }
+            else if (activiteit.Soort == ActiviteitSoort.Cultuur)
+            {
+                LblSpecifiek.Content = "Sector";
+                TxtSpecifiek.Text = ((CultuurActiviteit)activiteit).Sector?.ToString() ?? "";
+            }
+            else if (activiteit.Soort == ActiviteitSoort.Sport)
+            {
+                LblSpecifiek.Content = "Moeilijkheid";
+                TxtSpecifiek.Text = ((SportActiviteit)activiteit).Moeilijkheid?.ToString() ?? "";
+            }
+        }
+
+        private void BtnAnnuleren_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new OrganiseerPage());
+        }
+
+        private void BtnVerwijderen_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                activiteit.DeleteFromDb();
+                NavigationService.Navigate(new OrganiseerPage());
+            }
+            catch (Exception ex)
+            {
+                LblError.Content = $"Error: {ex.Message}";
+            }
         }
     }
 }
